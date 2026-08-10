122e17b02dcb823e4362d818ebd096e8b84cc5f1
Muhammad Jaffer <mjaffer720@gmail.com>
2026-08-10 15:21:17 +0500

    chore:modify resume file add latest.

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -76,7 +76,7 @@ export function HeroSection() {
               className="flex flex-wrap gap-4 justify-center"
             >
               <ButtonLink
-                href="/Muhammad-Jaffer-CV-26.3.pdf"
+                href={personalInfo.Resume}
                 newTab={true}
               >
                 <Download /> Download Resume
